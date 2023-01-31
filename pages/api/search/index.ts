1b096d6e329b9b14cbffbfa95421b8aed66265a8
alexmunoz1 <amunoz@filadd.com>
2023-01-31 19:18:22 -0300

    Remove 'authMiddleware' in search endpoint

--- pages/api/search/index.ts
+++ pages/api/search/index.ts
@@ -6,28 +6,28 @@ import method from "micro-method-router";
 import * as yup from "yup";
 
 const querySchema = yup
-   .object()
-   .shape({
-      query: yup.string().required(),
-      limit: yup.string().required(),
-      offset: yup.string().required(),
-   })
-   .noUnknown(true)
-   .strict();
+  .object()
+  .shape({
+    query: yup.string().required(),
+    limit: yup.string().required(),
+    offset: yup.string().required(),
+  })
+  .noUnknown(true)
+  .strict();
 
 async function getProducts(req: NextApiRequest, res: NextApiResponse) {
-   try {
-      const { query, limit, offset } = req.query;
-      const products = await searchProductByName(
-         query as string,
-         limit as string,
-         offset as string
-      );
-      res.status(200).json(products);
-   } catch (err) {
-      res.status(500).json({ err });
-   }
+  try {
+    const { query, limit, offset } = req.query;
+    const products = await searchProductByName(
+      query as string,
+      limit as string,
+      offset as string
+    );
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ err });
+  }
 }
 
-const handler = method({ get: authMiddleware(getProducts) });
+const handler = method({ get: getProducts });
 export default valideteMiddleware(querySchema, handler, "query");
